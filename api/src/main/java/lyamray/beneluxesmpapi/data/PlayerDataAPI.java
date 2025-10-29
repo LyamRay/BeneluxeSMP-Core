7702e21c07bdf95c3fc3c9d587d32a8ad9d78e2b
LyamRay <lyam.ray410@gmail.com>
2025-10-29 19:39:37 +0100

    Updated.

--- api/src/main/java/lyamray/beneluxesmpapi/data/PlayerDataAPI.java
+++
@@ -1,8 +0,0 @@
-package lyamray.beneluxesmpapi.data;
-
-import org.bukkit.entity.Player;
-
-public interface PlayerDataAPI {
-    int getCredits(Player player);
-    void setCredits(Player player, int amount);
-}
